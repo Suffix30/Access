ef15393cf2237cd587f22dff1a69f8f8cb580585
Gaspberry <49195535+Suffix30@users.noreply.github.com>
2024-09-01 19:30:58 -0400

    Update TheLock.js

--- pages/LandingPage/TheLock.js
+++ pages/LandingPage/TheLock.js
@@ -1,13 +1,15 @@
-document.getElementById('unlock-btn').addEventListener('click', function () {
-    const digit1 = document.getElementById('digit1').value;
-    const digit2 = document.getElementById('digit2').value;
-    const digit3 = document.getElementById('digit3').value;
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('unlock-btn').addEventListener('click', function () {
+        const digit1 = document.getElementById('digit1').value;
+        const digit2 = document.getElementById('digit2').value;
+        const digit3 = document.getElementById('digit3').value;
 
-    console.log(digit1, digit2, digit3);
+        console.log(digit1, digit2, digit3);
 
-    if (digit1 == 3 && digit2 == 1 && digit3 == 4) {
-        window.location.href = '../SelectionPage/selection.html';
-    } else {
-        alert('Incorrect combination, try again.');
-    }
+        if (digit1 == 3 && digit2 == 1 && digit3 == 4) {
+            window.location.href = '../SelectionPage/selection.html';
+        } else {
+            alert('Incorrect combination, try again.');
+        }
+    });
 });
