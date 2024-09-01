9b23f8486c7fe6ca7d295bdfee6642bea1857735
Gaspberry <49195535+Suffix30@users.noreply.github.com>
2024-09-01 19:18:03 -0400

    Update TheLock.js

--- pages/LandingPage/TheLock.js
+++ pages/LandingPage/TheLock.js
@@ -3,9 +3,9 @@ document.getElementById('unlock-btn').addEventListener('click', function () {
     const digit2 = document.getElementById('digit2').value;
     const digit3 = document.getElementById('digit3').value;
 
-    const correctDigit1 = 'XHUwMDMz';
-    const correctDigit2 = 'XHUwMDMx';
-    const correctDigit3 = 'XHUwMDM0';
+    const correctDigit1 = '\u0033';
+    const correctDigit2 = '\u0031';
+    const correctDigit3 = '\u0034';
 
     if (digit1 === correctDigit1 && digit2 === correctDigit2 && digit3 === correctDigit3) {
         window.location.href = '\u002e\u002e\u002f\u0053\u0065\u006c\u0065\u0063\u0074\u0069\u006f\u006e\u0050\u0061\u0067\u0065\u002f\u0073\u0065\u006c\u0065\u0063\u0074\u0069\u006f\u006e\u002e\u0068\u0074\u006d\u006c'; // "../SelectionPage/selection.html"
